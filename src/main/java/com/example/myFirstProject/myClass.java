9bc9a8d06acd305fa3df12b0d1332851b5520f80
Aman Shaikh <amanshaikh9156@gmail.com>
2026-04-13 23:36:49 +0530

    adding Project on Github

---
+++ src/main/java/com/example/myFirstProject/myClass.java
@@ -0,0 +1,13 @@
+package com.example.myFirstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class myClass {
+
+    @GetMapping("abc")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
